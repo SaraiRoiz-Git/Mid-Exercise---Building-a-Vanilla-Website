3ecd15d439f3a6cad80b1515f11ed1ea2467ec39
SaraiRoiz-Git <53699451+SaraiRoiz-Git@users.noreply.github.com>
2021-03-16 20:13:16 +0200

    fix button and vilidity

--- script.js
+++ script.js
@@ -4,23 +4,23 @@ function checkInputValidety() {
     document.getElementById('contact-us').classList.add('form-submitted');
 }
 
-var bunner1 = { title: 'yellow click&pick', src: '/images/ez.jpg' };
-var bunner2 = { title: 'שטחי מסחר פז סנטר', src: '/images/paz_250x110103131473.jpg' };
-var bunner3 = { title: 'טיפז', src: '/images/tip.jpg' };
-var bunnersList = [var1, var2, var3];
-function changeBunner() {
-    var title = document.querySelector('header-bunner');
-    var image = document.querySelector('image-bunner');
-    var currBunner;
-    while (true) {
-        setInterval(() => {
-            currBunner = bunnersList.pop;
-            title.innerHTML = currBunner.innerHTML;
-            image.src = currBunner.src;
-            bunnersList.push(changeBunner);
-        }, 5000);
-    }
-}
+// var bunner1 = { title: 'yellow click&pick', src: 'images/ez.jpg' };
+// var bunner2 = { title: 'שטחי מסחר פז סנטר', src: 'images/paz_250x110103131473.jpg' };
+// var bunner3 = { title: 'טיפז', src: 'images/tip.jpg' };
+// var bunnersList = [bunner1, bunner2, bunner3];
+// function changeBunner() {
+//     var title = document.querySelector('header-bunner');
+//     var image = document.querySelector('image-bunner');
+//     var currBunner;
+//     while (true) {
+//         setInterval(() => {
+//             currBunner = bunnersList.pop;
+//             title.innerHTML = currBunner.innerHTML;
+//             image.src = currBunner.src;
+//             bunnersList.push(changeBunner);
+//         }, 5000);
+//     }
+// }
 
-changeBunner()
+// changeBunner()
 
